66996cf3a61dbfceecea6dd81460822e483cde53
Shubhangi <shubhangi.ajegaonkar@gmail.com>
2025-09-25 00:13:22 +0200

    runfile

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -e
 
 echo "🧼 Cleaning reports..."
 rm -rf allure-results allure-report
-mkdir -p allure-results allure-report
+mkdir -p allure-results
 
 echo "🚀 Running tests..."
-pytest --headed --project=chromium
+pytest --headed --project=chromium --alluredir=allure-results
 
 echo "📊 Generating Allure report..."
 allure generate allure-results --clean -o allure-report
-
 echo "🌐 Opening Allure report..."
 allure open allure-report
